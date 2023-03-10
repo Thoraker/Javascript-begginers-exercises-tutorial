4766ab1da99bf2c10291e8a4b631d69ff72de084
Thoraker <pachecolobos.felix@gmail.com>
2023-03-10 05:35:58 +0000

    Ejercicio 24 terminado

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,4 +1,12 @@
-
+function sing() {
+    let lyrics = "let it be, ";
+    for (let i = 0; i <= 9; i++){
+        if(i===3) lyrics += 'whisper words of wisdom, ';
+        else if (i===9) lyrics += 'there will be an answer, let it be';
+        else lyrics += 'let it be, ';
+    }
+    return lyrics;
+}
 
 //Your code above ^^^
 
